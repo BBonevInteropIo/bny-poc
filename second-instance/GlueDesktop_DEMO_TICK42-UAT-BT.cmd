f3192474b0ab7284d0acf0ab295edd8e10e72e51
Boyko Bonev <boyko.bonev@tick42.com>
2023-09-04 11:52:04 +0300

    Added instructions to start second glue instance.

---
+++ second-instance/GlueDesktop_DEMO_TICK42-UAT-BT.cmd
@@ -0,0 +1,14 @@
+@echo off
+if not "%1" == "min" start /MIN cmd /c %0 min & exit/b >nul 2>&1
+set GLUE-ENV=UAT
+set GLUE-REGION=BT
+
+set GLUE42_HOME=%~dp0Glue42
+set GLUE_DESKTOP_HOME=%GLUE42_HOME%\GlueDesktop
+set GLUE_GW_LOG_DIR=%LOCALAPPDATA%\Tick42\UserData\%GLUE-ENV%-%GLUE-REGION%\logs\
+pushd %GLUE_DESKTOP_HOME%
+set NODE_OPTIONS="--tls-min-v1.0"
+if exist "config\configOverrides\system-%GLUE-ENV%-%GLUE-REGION%.json" start tick42-glue-desktop.exe -- config=config/system.json configOverrides config0=config/configOverrides/system-%GLUE-ENV%-%GLUE-REGION%.json
+if not exist "config\configOverrides\system-%GLUE-ENV%-%GLUE-REGION%.json" start tick42-glue-desktop.exe -- config=config/system.json
+popd
+exit /b
---
+++ second-instance/README.md
@@ -0,0 +1,7 @@
+## Setup second Glue Instance
+
+1. Move `GlueDesktop_DEMO_TICK42-UAT-BT.cmd` to `%LocalAppData%\Tick42\`
+2. Move `UAT-BT` folder to `%LocalAppData%\Tick42\UserData\`
+3. Move `configOverrides` folder to `%LocalAppData%\Tick42\Glue42\GlueDesktop\config`
+4. Move `systemApps-override` folder to `%LocalAppData%\Tick42\Glue42\GlueDesktop\config`
+5. Run `GlueDesktop_DEMO_TICK42-UAT-BT.cmd` to start the second glue instance
